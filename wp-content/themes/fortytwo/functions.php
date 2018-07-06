ecf831836b44801a2d975337b7e0d97422bd5eef
Anas Alhamdan <anas@bluekangaroo.co>
2018-07-07 02:31:12 +0300

    inital header

--- wp-content/themes/fortytwo/functions.php
+++ wp-content/themes/fortytwo/functions.php
@@ -28,3 +28,15 @@ function add_child_theme_textdomain() {
     load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
